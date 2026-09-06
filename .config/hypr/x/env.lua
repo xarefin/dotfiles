d03dee847125c663ed4ca15f0764d4e5f2c11c02
xarefin <arefinxakanda@gmail.com>
2026-09-07 02:19:54 +0600

    chore: update config files

--- .config/hypr/x/env.lua
+++ .config/hypr/x/env.lua
@@ -23,6 +23,8 @@ hl.env("XDG_SESSION_DESKTOP", "Hyprland")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1.00")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
+--hl.env("QT_QPA_PLATFORMTHEME", "kvantum")
+--hl.env("QT_STYLE_OVERRIDE", "kvantum")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 
 --nvidia
